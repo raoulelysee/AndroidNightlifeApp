0762f3359b0fce3adb9194c076d5c550317f3b6c
raoulelysee <raoulelysee@gmail.com>
2017-02-15 23:35:13 -0500

    Create Dashboard Activity - A first attempt of what it should look like

--- app/src/main/java/com/example/admin/mon1ernightlife/Welcome.java
+++ app/src/main/java/com/example/admin/mon1ernightlife/Welcome.java
@@ -3,6 +3,8 @@ package com.example.admin.mon1ernightlife;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.jar.Attributes;
@@ -12,6 +14,7 @@ import static com.example.admin.mon1ernightlife.SignUp.EXTRA_MESSAGE_USERNAME;
 public class Welcome extends AppCompatActivity {
 
     TextView message;
+    Button GoToMenuScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +26,15 @@ public class Welcome extends AppCompatActivity {
         String nameSigned = intent.getStringExtra(EXTRA_MESSAGE_USERNAME);
 
         message.setText(""+nameSigned);
+
+        GoToMenuScreen = (Button)findViewById(R.id.buttonNextPage);
+        GoToMenuScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent IntentToDasboardScreen = new Intent(Welcome.this,Dashboard.class);
+                startActivity(IntentToDasboardScreen);
+            }
+        });
     }
 }
